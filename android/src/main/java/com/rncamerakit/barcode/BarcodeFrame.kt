f2603c9faae055ded88f9b66a41dbe04ad70adb4
Roselle Ebarle <me@roselleebarle.com>
2021-09-28 01:35:38 +0800

    Test add border

--- android/src/main/java/com/rncamerakit/barcode/BarcodeFrame.kt
+++ android/src/main/java/com/rncamerakit/barcode/BarcodeFrame.kt
@@ -10,6 +10,7 @@ import com.rncamerakit.R
 
 class BarcodeFrame(context: Context) : View(context) {
     private var borderPaint: Paint = Paint()
+    private var firstBorderPaint: Paint = Paint()
     private var laserPaint: Paint = Paint()
     private var shadowPaint: Paint = Paint()
     var frameRect: Rect = Rect()
@@ -29,6 +30,9 @@ class BarcodeFrame(context: Context) : View(context) {
         borderPaint = Paint()
         borderPaint.style = Paint.Style.STROKE
         borderPaint.strokeWidth = STROKE_WIDTH.toFloat()
+        firstBorderPaint = Paint()
+        firstBorderPaint.style = Paint.Style.STROKE
+        firstBorderPaint.strokeWidth = FIRST_BORDER_STROKE_WIDTH.toFloat()
         laserPaint.style = Paint.Style.STROKE
         laserPaint.strokeWidth = STROKE_WIDTH.toFloat()
         shadowPaint.setColor(shadowColor);
@@ -37,6 +41,7 @@ class BarcodeFrame(context: Context) : View(context) {
                     BlurMaskFilter.Blur.NORMAL));
 
         borderMargin = context.resources.getDimensionPixelSize(R.dimen.border_length)
+        firstBorderMargin = context.resources.getDimensionPixelSize(R.dimen.border_length_bg)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -61,6 +66,15 @@ class BarcodeFrame(context: Context) : View(context) {
     }
 
     private fun drawBorder(canvas: Canvas) {
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), (frameRect.top + firstBorderMargin).toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), (frameRect.left + firstBorderMargin).toFloat(), frameRect.top.toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), frameRect.left.toFloat(), (frameRect.bottom - firstBorderMargin).toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), (frameRect.left + firstBorderMargin).toFloat(), frameRect.bottom.toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), (frameRect.right - firstBorderMargin).toFloat(), frameRect.top.toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), frameRect.right.toFloat(), (frameRect.top + firstBorderMargin).toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.bottom.toFloat(), frameRect.right.toFloat(), (frameRect.bottom - firstBorderMargin).toFloat(), firstBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.bottom.toFloat(), (frameRect.right - firstBorderMargin).toFloat(), frameRect.bottom.toFloat(), firstBorderPaint)
+        //Overlay
         canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), (frameRect.top + borderMargin).toFloat(), borderPaint)
         canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), (frameRect.left + borderMargin).toFloat(), frameRect.top.toFloat(), borderPaint)
         canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), frameRect.left.toFloat(), (frameRect.bottom - borderMargin).toFloat(), borderPaint)
@@ -87,6 +101,7 @@ class BarcodeFrame(context: Context) : View(context) {
     }
 
     companion object {
+        private const val FIRST_BORDER_STROKE_WIDTH = 5
         private const val STROKE_WIDTH = 10
         private const val ANIMATION_SPEED = 8
         private const val WIDTH_SCALE = 7
