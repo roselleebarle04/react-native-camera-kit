8f54a48872b981f032ed49a17bcc326a23f7c3b4
Roselle Ebarle <me@roselleebarle.com>
2021-09-28 02:54:26 +0800

    Add correct frame

--- android/src/main/java/com/rncamerakit/barcode/BarcodeFrame.kt
+++ android/src/main/java/com/rncamerakit/barcode/BarcodeFrame.kt
@@ -4,15 +4,13 @@ import android.content.Context
 import android.graphics.*
 import android.view.View
 import androidx.annotation.ColorInt
-import android.graphics.Color
 
 import com.rncamerakit.R
 
 class BarcodeFrame(context: Context) : View(context) {
+    private var mainBorderPaint: Paint = Paint()
     private var borderPaint: Paint = Paint()
-    private var firstBorderPaint: Paint = Paint()
     private var laserPaint: Paint = Paint()
-    private var shadowPaint: Paint = Paint()
     var frameRect: Rect = Rect()
 
     private var frameWidth = 0
@@ -21,27 +19,19 @@ class BarcodeFrame(context: Context) : View(context) {
     private var previousFrameTime = System.currentTimeMillis()
     private var laserY = 0
 
-    private var shadowColor = Color.Black;
-    private var offsetX = -25;
-    private var offsetY = 30;
-    private var blurRadius = 5;
-
     private fun init(context: Context) {
         borderPaint = Paint()
         borderPaint.style = Paint.Style.STROKE
         borderPaint.strokeWidth = STROKE_WIDTH.toFloat()
-        firstBorderPaint = Paint()
-        firstBorderPaint.style = Paint.Style.STROKE
-        firstBorderPaint.strokeWidth = FIRST_BORDER_STROKE_WIDTH.toFloat()
-        laserPaint.style = Paint.Style.STROKE
-        laserPaint.strokeWidth = STROKE_WIDTH.toFloat()
-        shadowPaint.setColor(shadowColor);
-        shadowPaint.setMaskFilter(new BlurMaskFilter(
-                    blurRadius /* shadowRadius */,
-                    BlurMaskFilter.Blur.NORMAL));
 
+        mainBorderPaint = Paint()
+        mainBorderPaint.color = Color.WHITE
+        mainBorderPaint.style = Paint.Style.STROKE
+        mainBorderPaint.strokeWidth = MAIN_BORDER_STROKE_WIDTH.toFloat()
+
+        laserPaint.style = Paint.Style.STROKE
+        laserPaint.strokeWidth = LASER_STROKE_WIDTH.toFloat()
         borderMargin = context.resources.getDimensionPixelSize(R.dimen.border_length)
-        firstBorderMargin = context.resources.getDimensionPixelSize(R.dimen.border_length_bg)
     }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
@@ -66,15 +56,11 @@ class BarcodeFrame(context: Context) : View(context) {
     }
 
     private fun drawBorder(canvas: Canvas) {
-        canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), (frameRect.top + firstBorderMargin).toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), (frameRect.left + firstBorderMargin).toFloat(), frameRect.top.toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), frameRect.left.toFloat(), (frameRect.bottom - firstBorderMargin).toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), (frameRect.left + firstBorderMargin).toFloat(), frameRect.bottom.toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), (frameRect.right - firstBorderMargin).toFloat(), frameRect.top.toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), frameRect.right.toFloat(), (frameRect.top + firstBorderMargin).toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.right.toFloat(), frameRect.bottom.toFloat(), frameRect.right.toFloat(), (frameRect.bottom - firstBorderMargin).toFloat(), firstBorderPaint)
-        canvas.drawLine(frameRect.right.toFloat(), frameRect.bottom.toFloat(), (frameRect.right - firstBorderMargin).toFloat(), frameRect.bottom.toFloat(), firstBorderPaint)
-        //Overlay
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), frameRect.bottom.toFloat(), mainBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), frameRect.right.toFloat(), frameRect.bottom.toFloat(), mainBorderPaint)
+        canvas.drawLine(frameRect.right.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), frameRect.top.toFloat(), mainBorderPaint)
+        canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), frameRect.right.toFloat(), frameRect.bottom.toFloat(), mainBorderPaint)
+
         canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), frameRect.left.toFloat(), (frameRect.top + borderMargin).toFloat(), borderPaint)
         canvas.drawLine(frameRect.left.toFloat(), frameRect.top.toFloat(), (frameRect.left + borderMargin).toFloat(), frameRect.top.toFloat(), borderPaint)
         canvas.drawLine(frameRect.left.toFloat(), frameRect.bottom.toFloat(), frameRect.left.toFloat(), (frameRect.bottom - borderMargin).toFloat(), borderPaint)
@@ -86,8 +72,7 @@ class BarcodeFrame(context: Context) : View(context) {
     }
 
     private fun drawLaser(canvas: Canvas, timeElapsed: Long) {
-        if (laserY > frameRect.bottom || laserY < frameRect.top) laserY = frameRect.top
-        canvas.drawLine((frameRect.left + STROKE_WIDTH).toFloat(), laserY.toFloat(), (frameRect.right - STROKE_WIDTH).toFloat(), laserY.toFloat(), shadowPaint)
+        if (laserY > (frameRect.bottom - STROKE_WIDTH) || laserY < frameRect.top) laserY = frameRect.top
         canvas.drawLine((frameRect.left + STROKE_WIDTH).toFloat(), laserY.toFloat(), (frameRect.right - STROKE_WIDTH).toFloat(), laserY.toFloat(), laserPaint)
         laserY += (timeElapsed / ANIMATION_SPEED).toInt()
     }
@@ -101,11 +86,12 @@ class BarcodeFrame(context: Context) : View(context) {
     }
 
     companion object {
-        private const val FIRST_BORDER_STROKE_WIDTH = 5
-        private const val STROKE_WIDTH = 10
+        private const val MAIN_BORDER_STROKE_WIDTH = 2.5
+        private const val LASER_STROKE_WIDTH = 2.5
+        private const val STROKE_WIDTH = 12
         private const val ANIMATION_SPEED = 8
         private const val WIDTH_SCALE = 7
-        private const val HEIGHT_SCALE = 5.5
+        private const val HEIGHT_SCALE = 2.75
     }
 
     init {
